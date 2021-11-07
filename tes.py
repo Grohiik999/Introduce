a9f698bfed9a65cb9b047f1de98f921903236336
Gigih <93849861+Grohiik999@users.noreply.github.com>
2021-11-07 09:57:00 +0700

    Add files via upload

---
+++ tes.py
@@ -0,0 +1,19 @@
+# tipe data string karena digunakan untuk menyatakan kalimat.
+nama = "Gigih Fajrul Rizky"
+# tipe data integer karena digunakan untuk menyatakan bilangan bulat.
+nim = 212410102052
+# tipe data integer karena digunakan untuk menyatakan bilangan bulat.
+umur = 18
+# tipe data integer karena digunakan untuk menyatakan bilangan bulat.
+tinggi_badan = 160
+# tipe data integer karena digunakan untuk menyatakan bilangan bulat.
+berat_badan = 50
+# tipe data boolean karena digunakan untuk menyatakan True or False.
+jomblo = True
+
+print(nama)
+print(nim)
+print(umur)
+print(tinggi_badan)
+print(berat_badan)
+print(jomblo)
